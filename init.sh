7db002981a9db996c4c5dbe8f0d073d13dd3826e
sharjeelbaig <dr.sharjeel.6@gmail.com>
2025-02-17 21:38:49 +0500

    initialize React + TypeScript + Vite project with basic configuration and dependencies

--- init.sh
+++ init.sh
@@ -1,1 +1,8 @@
-pip install opencv-python dlib imutils numpy scipy
+pip install flask
+pip install flask-cors
+pip install opencv-python
+pip install dlib
+pip install face_recognition
+pip install numpy
+pip install base64
+pip install scipy
